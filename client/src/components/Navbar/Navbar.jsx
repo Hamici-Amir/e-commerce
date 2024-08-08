bc63d8558788dd2f2e3db316b23b0fba705f68da
hatem12345th <h.taleb@esi-sba.dz>
2024-08-09 00:02:31 +0100

    Header

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom"
+import { NavFeatures } from "../../constants"
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { IoIosNotifications } from "react-icons/io";
+import { asserts } from "../../constants";
+
+export const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+  const [toogle, setToogle] = useState(true)
+    return (
+    <nav className="  h-20 w-full px-[100px] flex justify-between items-center ">
+
+            <div className=" text-orange-600   text-5xl font-extrabold  ">
+                    Tomato. 
+            </div>
+            <div className=" flex  items-center  gap-3   ">
+            {NavFeatures.map((item,index) => (
+                <Link key={index} to={"/"}>
+                        <div className={` px-3  transition-transform ${ menu == item ? "underline":""}  text-2xl text-[#49557e] `} 
+                            onClick={() => setMenu(item)}
+                        >
+                            {item}
+                        </div>
+                </Link>
+
+            ) )}
+            </div>
+            <div className=" w-[132px]  flex gap-4 items-center text-[#49557e] ">
+            <CiSearch  size={ toogle ? 80:40}   className=" cursor-pointer " />
+            {
+                       ! toogle && (<>
+                                <IoIosNotifications size={40} className="  cursor-pointer " />
+                                <button class=" cursor-pointer  text-white  font-bold  rounded-full">
+                                        <img src="https://lh3.googleusercontent.com/a/ACg8ocIB0yJT_ZshZKJZtvUQpPTRoZy2VjVyZ8w0cnYo6nvhTyHy-w=s288-c-no" alt="" width={40} className=" rounded-full"  />
+                                </button>
+                        </>)
+            }
+            {
+                toogle && 
+                <button class="bg-transparent hover:bg-orange-600  text-[#49557e] font-bold hover:text-white py-2 px-2.5 w-40 border border-orange-600  hover:border-transparent rounded-full "
+                        onClick={() => setToogle(!toogle)}
+                >
+                sign-in
+</button>
+            }
+            </div>
+        
+
+    </nav>
+  )
+}
---
+++ client/src/constants/index.js
@@ -0,0 +1,7 @@
+export const NavFeatures = ["home","menu","mobile-app","contact us"];
+
+import Logo from "../assets/background.png"
+
+export  const asserts ={
+    Logo
+}
