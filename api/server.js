561481e47a6b4137a34f73cfcd36d2237a3bb69b
hatem12345th <h.taleb@esi-sba.dz>
2024-08-24 17:44:18 +0100

    Signup && login functions

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 // nodejs packages 
 import express from "express"
 import dotenv from "dotenv";
-
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 
 // Data base 
@@ -17,6 +18,8 @@ const app = express();
 const port = process.env.port || 5000;
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 
 app.use("/api/auth/",AuthRouter);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,14 @@
 import { Route ,Routes  } from "react-router-dom"
 import { EmailVerification, ErrorRoute, Home, Login, Signup } from "./pages"
 import { Footer, Navbar } from "./components/Navbar"
+import {useAuthStore} from "./zustand/store"
+import LoadingSpinner from "./components/Loading/LoadingSpinner"
+import { Toaster } from "react-hot-toast";
 
 
 function App() {
-
+  
+  if(!true) return <LoadingSpinner />
 
   return (
     <>
@@ -20,14 +24,14 @@ function App() {
 
           <Route path="/sign-in" element={<Login />}  />
           <Route path="/sign-up" element={<Signup />}  /> 
-          <Route path="/verifiy" element={<EmailVerification />} />
+          <Route path="/verify-email" element={<EmailVerification />} />
           <Route path="/*" element={<ErrorRoute /> } />     
 
 
         </Routes>
   
        
-    
+            <Toaster />
    
      
     </>
